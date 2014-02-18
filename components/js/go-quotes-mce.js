3e17f69602bd30c1d3a83edc890914937c3723cb
Stephen Page <stephenjpage@gmail.com>
2014-02-18 09:13:09 -0500

    Add conditional for inline quotes

--- components/js/go-quotes-mce.js
+++ components/js/go-quotes-mce.js
@@ -4,7 +4,11 @@
 			$.each( go_quotes, function( quote_type ) {
 				console_log(quote_type);
 				var mceCommand = 'mce' + quote_type,
-					shortcode_open = '[' + quote_type + ' attribution="" person=""]',
+					shortcode_open = '[' + quote_type
+					if ( quote_type != 'quote' ) {
+						shortcode_open += ' attribution=""';
+					}//end if
+					shortcode_open += ' person=""]',
 					shortcode_close = '[/' + quote_type + ']';
 
 				ed.addCommand(mceCommand, function(){
@@ -12,9 +16,9 @@
 
 					if( content ) {
 						tinyMCE.activeEditor.selection.setContent( shortcode_open + content + shortcode_close );
-					} else {
+					} else { //end if
 						tinyMCE.activeEditor.selection.setContent( shortcode_open );
-					}
+					}//end else
 				});
 
 				ed.addButton( quote_type, {
